204f6d10a6dfe98a0746622b2ec7f69d1f1a09e6
rjangid631 <rohanjangid631@gmail.com>
2026-02-07 19:53:12 +0530

    updated whatsapp code

--- Management/presentation/UI.py
+++ Management/presentation/UI.py
@@ -378,8 +378,6 @@ from django.utils import timezone
 from django.contrib import messages
 from django.core.mail import send_mail
 from django.conf import settings
-
-from tickets.models import Tickets, TaskAssignment, UserProfile
 from Management.utils import send_whatsapp_ticket
 
 
